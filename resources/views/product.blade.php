9a4cb0c1705dd1b629a9d912cd98e05f651f970d
Ilya Skidonenko <fastqvver@gmail.com>
2021-02-05 17:02:47 +0300

    fix product link, added more info to product page

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -11,7 +11,7 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
 
-                <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+                <div class="mt-8 dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1">
                         <div class="p-6">
                             <div class="flex items-center">
@@ -21,10 +21,15 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                                    <a href="{{ $product->url }}">
+                                        <img src="{{ $product->picture }}" alt="{{ $product->name }}">
+                                    </a>
+                                </div>
+                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
                                     {{ $product->description }}
                                 </div>
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    {{ $product->price . ' ' . $product->currency }}
+                                    {{ $product->human_price }}
                                 </div>
                             </div>
                         </div>
